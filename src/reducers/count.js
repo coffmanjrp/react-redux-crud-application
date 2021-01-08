20fa829a371b55e4a84be5430976c2483e889f8b
coffmanjrp <coffmanjrp@gmail.com>
2021-01-07 21:15:06 -0800

    implement reducer

---
+++ src/reducers/count.js
@@ -0,0 +1,18 @@
+import { INCREMENT, DECREMENT } from '../actions/index';
+
+const initialState = {
+  value: 0,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { value: state.value + 1 };
+      break;
+    case DECREMENT:
+      return { value: state.value - 1 };
+    default:
+      return state;
+      break;
+  }
+};
