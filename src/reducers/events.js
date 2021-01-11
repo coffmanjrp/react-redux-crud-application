e5045ce055f1c822ec53e5e02a6f6b202843a724
coffmanjrp <coffmanjrp@gmail.com>
2021-01-10 18:32:12 -0800

    implement read event

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
-import { READ_EVENTS, DELETE_EVENT } from '../actions/index';
+import { READ_EVENTS, READ_EVENT, DELETE_EVENT } from '../actions/index';
 
 /* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id');
+    case READ_EVENT:
+      const data = action.response.data;
+      return { ...events, [data.id]: data };
     case DELETE_EVENT:
       delete events[action.id];
       return { ...events };
